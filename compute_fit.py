685926550ffbfe577580f3d0affa0304b9f1049f
Maurizio Tomasi <ziotom78@gmail.com>
2020-09-24 16:35:11 +0200

    Use the simulation framework everywhere

--- compute_fit.py
+++ compute_fit.py
@@ -1,29 +1,85 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
+from dataclasses import dataclass
 import json
 from pathlib import Path
+from shutil import copyfile
 import sys
+from typing import Dict, Any, List, Union
 
 from tqdm import tqdm
+
+import litebird_sim as lbs
 
 import healpy
 import numpy as np
 from scipy import optimize, interpolate, integrate
-import tomlkit
 
-from simulate import DetectorInfo
+import matplotlib
 
-SED_FILE = {
-    "jupiter": "jupiter_sed_ghz_k.csv",
-}
+matplotlib.use("Agg")
+import matplotlib.pylab as plt
 
-PLANET_RADII_M = {
-    "jupiter": 7.1492e7,
-    "mars": 3.389e6,
-    "uranus": 2.5362e7,
-    "neptune": 2.4622e7,
-}
+
+@dataclass
+class Parameters:
+    planet_name: str
+    sed_file_name: str
+    planet_radius_m: float
+    scanning_simulation: Path
+    detector: lbs.DetectorInfo
+    num_of_mc_runs: int
+
+    def __post_init__(self):
+        self.sed_file_name = Path(self.sed_file_name)
+        self.scanning_simulation = Path(self.scanning_simulation)
+
+
+def read_detector(parameters: Dict[str, Any], imo: lbs.Imo):
+    if "channel_obj" in parameters:
+        detobj = lbs.FreqChannelInfo.from_imo(
+            imo, parameters["channel_obj"]
+        ).get_boresight_detector()
+
+    elif "detector_obj" in parameters:
+        detobj = lbs.DetectorInfo.from_imo(imo, parameters["detector_obj"])
+    else:
+        detobj = lbs.DetectorInfo()
+
+    for param_name in (
+        "name",
+        "wafer",
+        "pixel",
+        "pixtype",
+        "channel",
+        "sampling_rate_hz",
+        "fwhm_arcmin",
+        "ellipticity",
+        "net_ukrts",
+        "fknee_mhz",
+        "fmin_hz",
+        "alpha",
+        "pol",
+        "orient",
+        "bandwidth_ghz",
+        "bandcenter_ghz",
+    ):
+        if param_name in parameters:
+            setattr(detobj, param_name, parameters[param_name])
+
+    return detobj
+
+
+def load_parameters(sim: lbs.Simulation) -> Parameters:
+    return Parameters(
+        planet_name=sim.parameters["planet"]["planet_name"],
+        sed_file_name=sim.parameters["planet"]["sed_file_name"],
+        planet_radius_m=sim.parameters["planet"]["planet_radius_m"],
+        scanning_simulation=sim.parameters["planet"]["scanning_simulation"],
+        detector=read_detector(sim.parameters["detector"], sim.imo),
+        num_of_mc_runs=sim.parameters["simulation"].get("num_of_mc_runs", 20),
+    )
 
 
 def beamfunc(pixel_theta, fwhm_arcmin, amplitude=1.0):
@@ -37,46 +93,40 @@ def calc_beam_solid_angle(fwhm_arcmin):
 
 
 def main(data_path: Path):
-    with next(data_path.glob("*.toml")).open("rt") as inpf:
-        sim_parameters = tomlkit.loads("".join(inpf.readlines()))
+    sim = lbs.Simulation(
+        parameter_file=sys.argv[1],
+        name="In-flight estimation of the beam properties",
+        description="""
+This report contains the result of a simulation of the reconstruction
+of in-flight beam parameters, assuming a scanning strategy and some
+noise/optical properties of a detector.
+""",
+    )
 
-    with (data_path / "parameters.json").open("rt") as inpf:
-        d = json.load(inpf)["detector"]
-        det = DetectorInfo(
-            name="boresight",
-            fwhm_arcmin=d["fwhm_arcmin"],
-            net_ukrts=d["net_ukrts"],
-            bandwidth_ghz=d["bandwidth_ghz"],
-            bandcenter_ghz=d["bandcenter_ghz"],
-            sampling_rate_hz=d["sampling_rate_hz"],
-        )
+    params = load_parameters(sim)
 
-    planet_name = sim_parameters["planet_scanning"]["planet_name"]
+    det = read_detector(sim.parameters["detector"], sim.imo)
+
+    # TODO: This should be done by the framework
+    copyfile(
+        src=params.sed_file_name, dst=sim.base_path / params.sed_file_name.name,
+    )
 
     # Calculate the brightness temperature of the planet over the band
-    sed_data = np.loadtxt(SED_FILE[planet_name], delimiter=",")
+    sed_data = np.loadtxt(params.sed_file_name, delimiter=",")
     sed_fn = interpolate.interp1d(sed_data[:, 0], sed_data[:, 1])
     planet_temperature_k = (
         integrate.quad(
             sed_fn,
-            det.bandcenter_ghz - det.bandwidth_ghz / 2,
-            det.bandcenter_ghz + det.bandwidth_ghz / 2,
+            params.detector.bandcenter_ghz - params.detector.bandwidth_ghz / 2,
+            params.detector.bandcenter_ghz + params.detector.bandwidth_ghz / 2,
         )[0]
-        / det.bandwidth_ghz
+        / params.detector.bandwidth_ghz
     )
-    planet_radius_m = PLANET_RADII_M[planet_name]
     beam_solid_angle = calc_beam_solid_angle(fwhm_arcmin=det.fwhm_arcmin)
-    sampling_time_s = 1.0 / det.sampling_rate_hz
-    print(f"Planet temperature: {planet_temperature_k:.1f} K")
-    print(f"Planet radius: {planet_radius_m:.2e} m")
-    print(f"FWHM: {det.fwhm_arcmin:.2f} arcmin")
-    print(f"Beam solid angle Ω: {beam_solid_angle:.2e} sterad")
-    print(f"White noise level: {det.net_ukrts * 1e-6:.2e} K·√s")
-    print(f"Sampling time: {sampling_time_s:.3f} s")
+    sampling_time_s = 1.0 / params.detector.sampling_rate_hz
 
-    input_map_file_name = (
-        data_path / sim_parameters["planet_scanning"]["output_map_file_name"]
-    )
+    input_map_file_name = params.scanning_simulation / "map.fits.gz"
 
     hit_map, time_map_s, dist_map_m2 = healpy.read_map(
         input_map_file_name, field=(0, 1, 2), verbose=False, dtype=np.float32
@@ -84,24 +134,63 @@ def main(data_path: Path):
     nside = healpy.npix2nside(len(dist_map_m2))
     pixel_theta, pixel_phi = healpy.pix2ang(nside, np.arange(len(hit_map)))
 
-    gamma_map = beamfunc(pixel_theta, det.fwhm_arcmin)
+    gamma_map = beamfunc(pixel_theta, params.detector.fwhm_arcmin)
 
-    mask = (hit_map > 0.0) & (pixel_theta < np.deg2rad(3 * det.fwhm_arcmin / 60.0))
-    print(f"Pixels used in the fit: {len(mask[mask])} / {len(mask)}")
-    print(f"Overall integration time for γ: {np.sum(time_map_s[mask])} s")
+    mask = (hit_map > 0.0) & (
+        pixel_theta < np.deg2rad(3 * params.detector.fwhm_arcmin / 60.0)
+    )
+    assert hit_map[mask].size > 0, "no data available for the fit"
+
+    sim.append_to_report(
+        """
+
+## Detector properties
+
+Parameter | Value
+--------- | -----------------
+Channel | {{det.channel}}
+Sampling time | {{ "%.3f"|format(sampling_time_s) }} s
+NET | {{det.net_ukrts}} μK·√s
+Bandwidth | {{det.bandwidth_ghz}} GHz
+Band center | {{det.bandcenter_ghz}} GHz
+FWHM | {{det.fwhm_arcmin}} arcmin
+Beam solid angle | {{beam_solid_angle}} sterad
+
+## Properties of the planet
+
+Parameter | Value
+--------- | ----------------
+Brightness temperature | {{ "%.1f"|format(planet_temperature_k) }} K
+Effective radius | {{ "%.0e"|format(params.planet_radius_m) }} m
+
+## Beam scanning
+
+Parameter | Value
+--------- | ----------------
+Pixels used in the fit | {{ num_of_pixels_used }}
+Integration time | {{ integration_time_s }} s
+""",
+        det=params.detector,
+        params=params,
+        beam_solid_angle=beam_solid_angle,
+        sampling_time_s=sampling_time_s,
+        planet_temperature_k=planet_temperature_k,
+        num_of_pixels_used=len(mask[mask]),
+        integration_time_s=np.sum(time_map_s[mask]),
+    )
 
     error_amplitude_map = (
         beam_solid_angle
-        * (det.net_ukrts * 1e-6)
+        * (params.detector.net_ukrts * 1e-6)
         / (
             np.pi
-            * (planet_radius_m ** 2)
+            * (params.planet_radius_m ** 2)
             * planet_temperature_k
             * np.sqrt(sampling_time_s)
         )
     ) * dist_map_m2
 
-    fwhm_estimates_arcmin = np.empty(20)
+    fwhm_estimates_arcmin = np.empty(params.num_of_mc_runs)
     ampl_estimates = np.empty(len(fwhm_estimates_arcmin))
     for i in tqdm(range(len(fwhm_estimates_arcmin))):
         noise_gamma_map = gamma_map + error_amplitude_map * np.random.randn(
@@ -112,16 +201,48 @@ def main(data_path: Path):
             beamfunc,
             pixel_theta[mask],
             noise_gamma_map[mask],
-            p0=[det.fwhm_arcmin, 1.0],
+            p0=[params.detector.fwhm_arcmin, 1.0],
         )
 
         fwhm_estimates_arcmin[i] = best_fit[0]
         ampl_estimates[i] = best_fit[1]
 
-    print(
-        f"FWHM: {np.mean(fwhm_estimates_arcmin):.3f} ± {np.std(fwhm_estimates_arcmin):.3f} arcmin"
+    fwhm_fig, fwhm_ax = plt.subplots()
+    fwhm_ax.hist(fwhm_estimates_arcmin)
+    fwhm_ax.set_xlabel("FWHM [arcmin]")
+    fwhm_ax.set_ylabel("Counts")
+
+    ampl_fig, ampl_ax = plt.subplots()
+    ampl_ax.hist(ampl_estimates)
+    ampl_ax.set_xlabel("AMPL [arcmin]")
+    ampl_ax.set_ylabel("Counts")
+
+    sim.append_to_report(
+        """
+## Results of the Monte Carlo simulation
+
+Parameter  | Value
+---------- | -----------------
+# of runs  | {{ num_of_runs }}
+FWHM       | {{"%.3f"|format(fwhm_arcmin)}} ± {{"%.3f"|format(fwhm_err)}} arcmin
+γ0         | {{"%.3f"|format(ampl)}} ± {{"%.3f"|format(ampl_err)}} arcmin
+
+![](fwhm_distribution.svg)
+
+![](ampl_distribution.svg)
+""",
+        figures=[
+            (fwhm_fig, "fwhm_distribution.svg"),
+            (ampl_fig, "ampl_distribution.svg"),
+        ],
+        num_of_runs=len(fwhm_estimates_arcmin),
+        fwhm_arcmin=np.mean(fwhm_estimates_arcmin),
+        fwhm_err=np.std(fwhm_estimates_arcmin),
+        ampl=np.mean(ampl_estimates),
+        ampl_err=np.std(ampl_estimates),
     )
-    print(f"Amplitude: {np.mean(ampl_estimates)} ± {np.std(ampl_estimates)}")
+
+    sim.flush()
 
 
 if __name__ == "__main__":
--- simulate_scanning_strategy.py
+++ simulate_scanning_strategy.py
@@ -11,14 +11,8 @@ import warnings
 import json
 import logging as log
 import sys
-from typing import Dict, Any, List, Union
+from typing import Dict, Any, List
 
-import matplotlib
-
-matplotlib.use("Agg")
-import matplotlib.pylab as plt
-
-from numba import njit
 import numpy as np
 from tqdm import tqdm
 import litebird_sim as lbs
@@ -31,85 +25,41 @@ from astropy.coordinates import (
     solar_system_ephemeris,
 )
 
+import matplotlib
 
-@dataclass
-class DetectorInfo:
-    name: str = ""
-    wafer: Union[str, None] = None
-    pixel: Union[int, None] = None
-    pixtype: Union[str, None] = None
-    channel: Union[str, None] = None
-    sampling_rate_hz: float = 0.0
-    fwhm_arcmin: float = 0.0
-    ellipticity: float = 0.0
-    net_ukrts: float = 0.0
-    fknee_mhz: float = 0.0
-    fmin_hz: float = 0.0
-    alpha: float = 0.0
-    pol: Union[str, None] = None
-    orient: Union[str, None] = None
-    quat: Any = np.array([0.0, 0.0, 0.0, 1.0])
-    bandwidth_ghz: float = 0.0
-    bandcenter_ghz: float = 0.0
-
-    @staticmethod
-    def from_dict(d: Dict[str, Any]):
-        return DetectorInfo(**d)
-
-    @staticmethod
-    def from_imo(imo: lbs.Imo, objurl):
-        return DetectorInfo.from_dict(imo.query(objurl).metadata)
+matplotlib.use("Agg")
+import matplotlib.pylab as plt
 
 
 @dataclass
-class ChannelInfo:
-    channel: str
-    bandcenter_ghz: float
-    bandwidth_ghz: float
-    net_detector_ukrts: float
-    net_channel_ukrts: float
-    pol_sensitivity_channel_ukarcmin: float
-    fwhm_arcmin: float
-    fknee_mhz: float
-    fmin_hz: float
-    alpha: float
-    number_of_detectors: int
-    detector_names: List[str]
+class Parameters:
+    spin_boresight_angle_deg: float
+    planet_name: str
+    spin2ecl_delta_time_s: float
+    detector_sampling_rate_hz: float
+    radii_deg: List[float]
+    output_nside: int = 1024
+    output_map_file_name: str = "map.fits.gz"
+    output_table_file_name: str = "observation_time_table.txt"
 
-    @staticmethod
-    def from_dict(d):
-        return ChannelInfo(
-            channel=d["channel"],
-            bandcenter_ghz=d["bandcenter"],
-            bandwidth_ghz=d["bandwidth"],
-            net_detector_ukrts=d["net_detector_ukrts"],
-            net_channel_ukrts=d["net_channel_ukrts"],
-            pol_sensitivity_channel_ukarcmin=d["pol_sensitivity_channel_uKarcmin"],
-            fwhm_arcmin=d["fwhm_arcmin"],
-            fknee_mhz=d["fknee_mhz"],
-            fmin_hz=d["fmin_hz"],
-            alpha=d["alpha"],
-            number_of_detectors=d["number_of_detectors"],
-            detector_names=d["detector_names"],
-        )
 
-    @staticmethod
-    def from_imo(imo, objref):
-        obj = imo.query(objref)
-        return ChannelInfo.from_dict(obj.metadata)
+def load_parameters(sim: lbs.Simulation) -> Parameters:
+    planet_params = sim.parameters["planet_scanning"]
 
-    def get_boresight_detector(self) -> DetectorInfo:
-        return DetectorInfo(
-            name="mock",
-            channel=self.channel,
-            fwhm_arcmin=self.fwhm_arcmin,
-            net_ukrts=self.net_detector_ukrts,
-            fknee_mhz=self.fknee_mhz,
-            fmin_hz=self.fmin_hz,
-            alpha=self.alpha,
-            bandwidth_ghz=self.bandwidth_ghz,
-            bandcenter_ghz=self.bandcenter_ghz,
-        )
+    return Parameters(
+        spin_boresight_angle_deg=sim.parameters["scanning_strategy"][
+            "spin_boresight_angle_deg"
+        ],
+        planet_name=planet_params["planet_name"],
+        spin2ecl_delta_time_s=planet_params["spin2ecl_delta_time_s"],
+        detector_sampling_rate_hz=planet_params["sampling_rate_hz"],
+        radii_deg=planet_params.get(
+            "radii_deg", [0.1, 0.2, 0.5, 1, 5, 10, 20, 40, 60, 90, 135, 180]
+        ),
+        output_nside=planet_params["output_nside"],
+        output_map_file_name=sim.base_path / "map.fits.gz",
+        output_table_file_name=sim.base_path / "observation_time_table.txt",
+    )
 
 
 def time_per_radius(time_map_s, angular_radius_rad):
@@ -152,65 +102,19 @@ def read_scanning_strategy(parameters: Dict[str, Any], imo: lbs.Imo, start_time)
     return sstr
 
 
-def read_detector(parameters: Dict[str, Any], imo: lbs.Imo):
-    if "channel_obj" in parameters:
-        detobj = ChannelInfo.from_imo(
-            imo, parameters["channel_obj"]
-        ).get_boresight_detector()
-
-    elif "detector_obj" in parameters:
-        detobj = DetectorInfo.from_imo(imo, parameters["detector_obj"])
-    else:
-        detobj = DetectorInfo()
-
-    for param_name in (
-        "name",
-        "wafer",
-        "pixel",
-        "pixtype",
-        "channel",
-        "sampling_rate_hz",
-        "fwhm_arcmin",
-        "ellipticity",
-        "net_ukrts",
-        "fknee_mhz",
-        "fmin_hz",
-        "alpha",
-        "pol",
-        "orient",
-        "bandwidth_ghz",
-        "bandcenter_ghz",
-    ):
-        if param_name in parameters:
-            setattr(detobj, param_name, parameters[param_name])
-
-    return detobj
-
-
 def main():
     warnings.filterwarnings("ignore", category=ErfaWarning)
 
     sim = lbs.Simulation(
         parameter_file=sys.argv[1],
-        name="In-flight beam",
+        name="Observation of planets",
         description="""
-    This is a simulation of in-flight beam reconstruction.
-    """,
+This report contains the result of a simulation of the observation
+of the sky, particularly with respect to the observation of planets.
+""",
     )
 
-    planet_params = sim.parameters["planet_scanning"]
-
-    # Initialize variables with the values in the TOML file
-    SPIN_BORESIGHT_ANGLE_DEG = sim.parameters["scanning_strategy"][
-        "spin_boresight_angle_deg"
-    ]
-
-    PLANET_NAME = planet_params["planet_name"]
-    SPIN2ECL_DELTA_TIME_S = planet_params["spin2ecl_delta_time_s"]
-    DETECTOR_SAMPLING_RATE_HZ = planet_params["sampling_rate_hz"]
-    OUTPUT_NSIDE = planet_params["output_nside"]
-    OUTPUT_MAP_FILE_NAME = sim.base_path / planet_params["output_map_file_name"]
-    OUTPUT_TABLE_FILE_NAME = sim.base_path / planet_params["output_table_file_name"]
+    params = load_parameters(sim)
 
     if lbs.MPI_ENABLED:
         log.info("Using MPI with %d processes", lbs.MPI_COMM_WORLD.size)
@@ -222,21 +126,16 @@ def main():
         sim.parameters["scanning_strategy"], sim.imo, sim.start_time
     )
     sim.generate_spin2ecl_quaternions(
-        scanning_strategy=scanning_strategy, delta_time_s=SPIN2ECL_DELTA_TIME_S,
+        scanning_strategy=scanning_strategy, delta_time_s=params.spin2ecl_delta_time_s,
     )
 
     log.info("Creating the observations")
     instr = lbs.Instrument(
-        name="instrum", spin_boresight_angle_deg=SPIN_BORESIGHT_ANGLE_DEG
+        name="instrum", spin_boresight_angle_deg=params.spin_boresight_angle_deg
     )
-    detectors = [
-        read_detector(detdef, sim.imo) for detdef in sim.parameters["detectors"]
-    ]
-    assert len(detectors) == 1, "Only one detector is allowed in this simulation"
-
-    detectors[0].sampling_rate_hz = DETECTOR_SAMPLING_RATE_HZ
+    detector = lbs.DetectorInfo(sampling_rate_hz=params.detector_sampling_rate_hz)
     sim.create_observations(
-        detectors=[detectors[0]],
+        detectors=[detector],
         num_of_obs_per_detector=sim.parameters["simulation"]["num_of_obs_per_detector"],
     )
 
@@ -244,8 +143,8 @@ def main():
     # Here begins the juicy part
 
     log.info("The loop starts on %d processes", lbs.MPI_COMM_WORLD.size)
-    sky_hitmap = np.zeros(healpy.nside2npix(OUTPUT_NSIDE), dtype=np.int32)
-    detector_hitmap = np.zeros(healpy.nside2npix(OUTPUT_NSIDE), dtype=np.int32)
+    sky_hitmap = np.zeros(healpy.nside2npix(params.output_nside), dtype=np.int32)
+    detector_hitmap = np.zeros(healpy.nside2npix(params.output_nside), dtype=np.int32)
     dist_map_m2 = np.zeros(len(detector_hitmap))
 
     iterator = tqdm
@@ -262,7 +161,7 @@ def main():
         # significantly. (In Ecliptic coordinates, Jupiter moves by
         # fractions of an arcmin over a time span of one hour)
         time0 = times[0]
-        icrs_pos = get_body_barycentric(PLANET_NAME, time0)
+        icrs_pos = get_body_barycentric(params.planet_name, time0)
         earth_pos = get_body_barycentric("earth", time0)
 
         # Compute the distance between the Earth and the planet
@@ -291,7 +190,7 @@ def main():
         # reference system into the detector's reference system
         quats = obs.get_ecl2det_quaternions(
             sim.spin2ecliptic_quats,
-            detector_quat=obs.detector.quat,
+            detector_quats=[detector.quat],
             bore2spin_quat=instr.bore2spin_quat,
         )
 
@@ -299,20 +198,20 @@ def main():
         det_vec = np.empty_like(ecl_vec)
 
         # Do the rotation!
-        lbs.all_rotate_vectors(det_vec, quats, ecl_vec)
+        lbs.all_rotate_vectors(det_vec, quats[0], ecl_vec)
 
         pixidx = healpy.vec2pix(
-            OUTPUT_NSIDE, det_vec[:, 0], det_vec[:, 1], det_vec[:, 2]
+            params.output_nside, det_vec[:, 0], det_vec[:, 1], det_vec[:, 2]
         )
         bincount = np.bincount(pixidx, minlength=len(detector_hitmap))
         detector_hitmap += bincount
         dist_map_m2 += bincount / ((4 * np.pi * (distance_m ** 2)) ** 2)
 
         pointings = obs.get_pointings(
-            sim.spin2ecliptic_quats, obs.detector.quat, instr.bore2spin_quat
-        )
+            sim.spin2ecliptic_quats, [detector.quat], instr.bore2spin_quat
+        )[0]
 
-        pixidx = healpy.ang2pix(OUTPUT_NSIDE, pointings[:, 0], pointings[:, 1])
+        pixidx = healpy.ang2pix(params.output_nside, pointings[:, 0], pointings[:, 1])
         bincount = np.bincount(pixidx, minlength=len(sky_hitmap))
         sky_hitmap += bincount
 
@@ -321,20 +220,19 @@ def main():
         detector_hitmap = lbs.MPI_COMM_WORLD.allreduce(detector_hitmap)
         dist_map_m2 = lbs.MPI_COMM_WORLD.allreduce(dist_map_m2)
 
-    time_map_s = detector_hitmap / DETECTOR_SAMPLING_RATE_HZ
+    time_map_s = detector_hitmap / params.detector_sampling_rate_hz
     dist_map_m2[dist_map_m2 > 0] = np.power(dist_map_m2[dist_map_m2 > 0], -0.5)
 
-    RADII_DEG = [0.1, 0.2, 0.5, 1, 5, 10, 20, 40, 60, 90, 135, 180]
-    OBS_TIME_PER_RADIUS_S = [
+    obs_time_per_radius_s = [
         time_per_radius(time_map_s, angular_radius_rad=np.deg2rad(radius_deg))
-        for radius_deg in RADII_DEG
+        for radius_deg in params.radii_deg
     ]
 
     if lbs.MPI_COMM_WORLD.rank == 0:
         # Create a plot of the observation time of the planet as a
         # function of the angular radius
         fig, ax = plt.subplots()
-        ax.loglog(RADII_DEG, OBS_TIME_PER_RADIUS_S)
+        ax.loglog(params.radii_deg, obs_time_per_radius_s)
         ax.set_xlabel("Radius [deg]")
         ax.set_ylabel("Observation time [s]")
 
@@ -345,56 +243,46 @@ def main():
         sim.append_to_report(
             """
 
-    ## Scanning strategy parameters
+## Scanning strategy parameters
 
-    Parameter | Value
-    --------- | --------------
-    Angle between the spin axis and the Sun-Earth axis | {{ sun_earth_angle_deg }} deg
-    Angle between the spin axis and the boresight | {{ bore_spin_angle_deg }} deg
-    Precession period | {{ precession_period_min }} min
-    Spin period | {{ spin_period_min }} min
+Parameter | Value
+--------- | --------------
+Angle between the spin axis and the Sun-Earth axis | {{ sun_earth_angle_deg }} deg
+Angle between the spin axis and the boresight | {{ params.spin_boresight_angle_deg }} deg
+Precession period | {{ precession_period_min }} min
+Spin period | {{ spin_period_min }} min
 
-    ## Detector properties
+## Observation of {{ params.planet_name | capitalize }}
 
-    Parameter | Value
-    --------- | -----------------
-    Channel | {{det.channel}}
-    FWHM | {{det.fwhm_arcmin}} arcmin
-    NET | {{det.net_ukrts}} μK·√s
-    Bandwidth | {{det.bandwidth_ghz}} GHz
-    Band center | {{det.bandcenter_ghz}} GHz
+![](detector_hitmap.png)
 
-    ## Observation of {{ planet_name | capitalize }}
+The overall time spent in the map is {{ overall_time_s }} seconds.
 
-    ![](detector_hitmap.png)
+The time resolution of the simulation was {{ delta_time_s }} seconds.
 
-    The overall time spent in the map is {{ overall_time_s }} seconds.
+Angular radius [deg] | Time spent [s]
+-------------------- | ------------------------
+{% for row in radius_vs_time_s -%}
+{{ "%.1f"|format(row[0]) }} | {{ "%.1f"|format(row[1]) }}
+{% endfor -%}
 
-    The time resolution of the simulation was {{ delta_time_s }} seconds.
+![](radius_vs_time.svg)
 
-    Angular radius [deg] | Time spent [s]
-    -------------------- | ------------------------
-    {% for row in radius_vs_time_s -%}
-    {{ "%.1f"|format(row[0]) }} | {{ "%.1f"|format(row[1]) }}
-    {% endfor -%}
-
-    ![](radius_vs_time.svg)
-
-    """,
+""",
             figures=[(plt.gcf(), "detector_hitmap.png"), (fig, "radius_vs_time.svg")],
-            planet_name=PLANET_NAME,
-            overall_time_s=np.sum(detector_hitmap) / DETECTOR_SAMPLING_RATE_HZ,
-            radius_vs_time_s=list(zip(RADII_DEG, OBS_TIME_PER_RADIUS_S)),
-            delta_time_s=1.0 / DETECTOR_SAMPLING_RATE_HZ,
+            params=params,
+            overall_time_s=np.sum(detector_hitmap) / params.detector_sampling_rate_hz,
+            radius_vs_time_s=list(zip(params.radii_deg, obs_time_per_radius_s)),
+            delta_time_s=1.0 / params.detector_sampling_rate_hz,
             sun_earth_angle_deg=np.rad2deg(scanning_strategy.spin_sun_angle_rad),
-            bore_spin_angle_deg=SPIN_BORESIGHT_ANGLE_DEG,
+            bore_spin_angle_deg=params.spin_boresight_angle_deg,
             precession_period_min=1.0 / (60.0 * scanning_strategy.precession_rate_hz),
             spin_period_min=1.0 / (60.0 * scanning_strategy.spin_rate_hz),
-            det=detectors[0],
+            det=detector,
         )
 
         healpy.write_map(
-            OUTPUT_MAP_FILE_NAME,
+            params.output_map_file_name,
             (detector_hitmap, time_map_s, dist_map_m2, sky_hitmap),
             coord="DETECTOR",
             column_names=["HITS", "OBSTIME", "SQDIST", "SKYHITS"],
@@ -404,8 +292,8 @@ def main():
         )
 
         np.savetxt(
-            OUTPUT_TABLE_FILE_NAME,
-            np.array(list(zip(RADII_DEG, OBS_TIME_PER_RADIUS_S))),
+            params.output_table_file_name,
+            np.array(list(zip(params.radii_deg, obs_time_per_radius_s))),
             fmt=["%.2f", "%.5e"],
         )
 
@@ -421,14 +309,8 @@ def main():
                         "spin_rate_hz": scanning_strategy.spin_rate_hz,
                         "start_time": time_value,
                     },
-                    "detector": {
-                        "sampling_rate_hz": DETECTOR_SAMPLING_RATE_HZ,
-                        "fwhm_arcmin": detectors[0].fwhm_arcmin,
-                        "net_ukrts": detectors[0].net_ukrts,
-                        "bandwidth_ghz": detectors[0].bandwidth_ghz,
-                        "bandcenter_ghz": detectors[0].bandcenter_ghz,
-                    },
-                    "planet": {"name": PLANET_NAME},
+                    "detector": {"sampling_rate_hz": params.detector_sampling_rate_hz},
+                    "planet": {"name": params.planet_name},
                 },
                 outf,
                 indent=2,
